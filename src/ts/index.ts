454153452f8f98d1a0d0e2a0c198d05cc49306c5
iSatVeerSingh <developer.satveer@gmail.com>
2023-08-05 17:01:24 +0530

    canvas setup

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,17 @@
 window.addEventListener("load", () => {
-  console.log("This is running very well")
-})
+  const root = document.getElementById("root");
+
+  const canvas = document.createElement("canvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  root?.appendChild(canvas);
+
+  const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!
+
+
+  ctx.beginPath()
+  ctx.rect(20, 20, 300, 400)
+  ctx.stroke()
+  ctx.closePath()
+});
