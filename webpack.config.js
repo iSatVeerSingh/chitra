684903105458d30eadc6c8de38284fe3ac3b140c
iSatVeerSingh <developer.satveer@gmail.com>
2023-08-05 16:50:09 +0530

    typescript setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,35 @@ import path from "path";
 const config = {
   entry: "./src/ts/index.ts",
   output: {
-    path: path.resolve('dist'),
-    filename: 'chitra.bundle.js'
+    path: path.resolve("dist"),
+    filename: "chitra.bundle.js",
+    clean: true
   },
   mode: "development",
   devServer: {
     port: 3000,
     hot: true,
     open: true,
+    static: "./public"
   },
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ]
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
 };
 
 export default config;
